70df1739bd1ecb38ce52bc35d641bdf597941d4a
netolucena <juracylucena@gmail.com>
2017-06-28 20:03:53 -0300

    iniciando laudos

---
+++ app/src/main/java/com/github/dicomflow/androiddicomflow/protocolo/services/ServiceFactory.java
@@ -0,0 +1,48 @@
+package com.github.dicomflow.androiddicomflow.protocolo.services;
+
+import com.github.dicomflow.androiddicomflow.protocolo.dicomobjects.Completed;
+import com.github.dicomflow.androiddicomflow.protocolo.dicomobjects.Data;
+import com.github.dicomflow.androiddicomflow.protocolo.dicomobjects.Result;
+import com.github.dicomflow.androiddicomflow.protocolo.dicomobjects.Url;
+import com.github.dicomflow.androiddicomflow.protocolo.services.request.RequestPut;
+import com.github.dicomflow.androiddicomflow.protocolo.services.request.RequestResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+
+/**
+ * Created by netolucena on 26/06/2017.
+ */
+
+public class ServiceFactory {
+    public static Service getService(ServiceTypes serviceType, Map<String, Object> params) {
+        Service service = null;
+        try {
+            if (serviceType == ServiceTypes.REQUESTPUT) {
+                String from = (String)params.get("from");
+                String requestType = (String)params.get("requestType");
+                Url url = (Url) params.get("url");
+                service = new RequestPut(from, requestType, url);
+            } else if (serviceType == ServiceTypes.REQUESTRESULT) {
+                String from = (String) params.get("from");
+                String filename = (String) params.get("filename");
+                String bytes = (String) params.get("bytes");
+                Data data = new Data(filename, bytes);
+
+                Completed completed = new Completed("1", "OK"); //TODO
+                Integer id = 1; //TODO
+                Result result = new Result(id, completed, data, null, null, null, null, null);
+                List resultList = new ArrayList<>();
+                resultList.add(result);
+                service = new RequestResult(from, resultList);
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return service;
+    }
+
+}
---
+++ app/src/main/java/com/github/dicomflow/androiddicomflow/protocolo/services/ServiceTypes.java
@@ -0,0 +1,9 @@
+package com.github.dicomflow.androiddicomflow.protocolo.services;
+
+/**
+ * Created by Neto on 27/06/2017.
+ */
+
+public enum ServiceTypes {
+    REQUESTPUT, REQUESTRESULT, STORAGESAVE, STORAGERESULT
+}
